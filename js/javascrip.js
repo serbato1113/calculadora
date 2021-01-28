85ab8b5e610ae6efc0d97599411a6ece36767056
unknown <serbato1113@gmail.com>
2021-01-28 15:28:14 -0500

    calculadora terminada con jss

--- js/javascrip.js
+++ js/javascrip.js
@@ -1,2 +1,64 @@
-//alert(bienvenidos);
-//swal ("bienvenidos");
+
+swal("Bienvenidos");
+
+
+function updateInput(newValue, clear = false) {
+    var div = document.getElementById('data');
+
+    div.value = clear ? newValue : `${div.value}${newValue}`;
+
+    /*if(clear){
+        div.value = newValue;
+    }else{
+        div.value = `${div.value}${newValue}`
+
+        // div.value + newValue
+    }*/
+
+}
+
+function printOperator(data) {
+    updateInput(` ${data.value} `);
+
+    //' ' + data.value + ' '
+}
+
+function printNumber(data) {
+    updateInput(data.value);
+}
+
+function clearInput() {
+    updateInput('', true);
+}
+
+function printOperation() {
+    var data = document.getElementById('data').value;
+    var array = validateOperator(data.split(' '));
+    updateInput(array, true);
+}
+
+function validateOperator(arrayData) {
+    var resultado;
+    var numero1,numero2;
+
+    numero1 = Number(arrayData[0]);
+    numero2 = Number(arrayData[2]);
+
+    switch (arrayData[1]) {
+        case '+':
+            resultado = numero1 + numero2;
+            break;
+        case '-':
+            resultado = numero1 - numero2;
+            break;
+        case '*':
+            resultado = numero1 * numero2; 
+            break;
+        case '/':
+            resultado = numero1 / numero2;
+            break;
+    }  
+
+    return resultado;
+
+}
